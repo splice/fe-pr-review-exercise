d3a88424f9c74b1d14454e2cbf4de921b6d890d9
Sue Lockwood <deathbearbrown@gmail.com>
2020-09-02 13:54:57 -0700

    Add demo field to form

--- src/app/modules/sample-providers/components/submission-form/submission-form.component.ts
+++ src/app/modules/sample-providers/components/submission-form/submission-form.component.ts
@@ -1,21 +1,73 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  ElementRef,
+  ViewChild,
+  HostListener,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+} from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   FormGroup,
   Validators,
+  NG_VALUE_ACCESSOR,
+  ControlValueAccessor,
 } from '@angular/forms';
 
 @Component({
   selector: 'app-submission-form',
   templateUrl: './submission-form.component.html',
   styleUrls: ['./submission-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SubmissionFormComponent,
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SubmissionFormComponent implements OnInit {
+export class SubmissionFormComponent implements OnInit, ControlValueAccessor {
   form: FormGroup;
+  // demo file name
+  file: File;
+
+  @ViewChild('player', { read: ElementRef }) player: ElementRef;
 
   @Output()
   submitForm = new EventEmitter<any>();
+
+  @Input()
+  onChange = (file: File) => {};
+
+  @HostListener('change', ['$event.target.files'])
+  emitFiles(event: FileList) {
+    this.file = event && event.item(0);
+    this.addAudioFile();
+    this.onChange(this.file);
+  }
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {}
+
+  /** @ignore */
+  writeValue(value) {
+    if (value === null && this.file) {
+      this.file = null;
+    }
+  }
+
+  addAudioFile() {
+    this.player.nativeElement.src = URL.createObjectURL(this.file);
+  }
 
   constructor(private formBuilder: FormBuilder) {}
 
